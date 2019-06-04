9efbe8c9ea65f0b3ac8e7f3591c47b16ca76d877
Anand J <anandj@cs.ubc.ca>
2019-06-03 17:12:17 -0700

    Redesign
    Using preprocessor directives to define intercepts

--- libvocl.cpp
+++ libvocl.cpp
@@ -1,23 +1,8 @@
 #include <stdio.h>
 #include "libvocl.h"
 
-/*
- ** Interposed Functions
- */
-void* dlsym(void *handle, const char *symbol)
-{
-    if (strcmp(symbol, "clWaitForEvents") == 0) {
-        return (void*)clWaitForEvents;
-    } else if (strcmp(symbol, "clEnqueueNDRangeKernel") == 0) {
-        return (void*)clEnqueueNDRangeKernel;
-    } else if (strcmp(symbol, "clSetKernelArg") == 0) {
-        return (void*)clSetKernelArg;
-    } else {
-        return real_dlsym(handle, symbol);
-    }
-}
-
-cl_int clEnqueueNDRangeKernel(cl_command_queue command_queue,
+cl_int voclEnqueueNDRangeKernel(fnclEnqueueNDRangeKernel real_fn,
+        cl_command_queue command_queue,
         cl_kernel kernel,
         cl_uint work_dim,
         const size_t *global_work_offset,
@@ -27,31 +12,65 @@ cl_int clEnqueueNDRangeKernel(cl_command_queue command_queue,
         const cl_event *event_wait_list,
         cl_event *event)
 {
-    static fnClEnqueueNDRangeKernel real_fn = (fnClEnqueueNDRangeKernel)real_dlsym(RTLD_NEXT, "clEnqueueNDRangeKernel");
-    cl_int ret = real_fn(command_queue, kernel, work_dim,
+    auto ret = real_fn(command_queue, kernel, work_dim,
             global_work_offset, global_work_size, local_work_size,
             num_events_in_wait_list, event_wait_list, event);
     int i = num_events_in_wait_list;
     do {
         fflush(stdout);
-        fprintf(stdout, "clEnqueueNDRangeKernel %lx: %lx %lx\n", kernel, (i-->0)?event_wait_list[i]:NULL, *event);
+        fprintf(stdout, "clEnqueueNDRangeKernel %lx: %lx %lx\n", kernel,
+                (i-- > 0) ? event_wait_list[i] : NULL, *event);
         fflush(stdout);
-    } while (i>0);
+    } while (i > 0);
+
+    return ret;
 }
 
-cl_int clWaitForEvents(cl_uint num_events, const cl_event *event_list)
+cl_int voclWaitForEvents(fnclWaitForEvents real_fn,
+        cl_uint num_events,
+        const cl_event *event_list)
 {
+    auto ret = real_fn(num_events, event_list);
     fprintf(stdout, "clWaitForEvents intercepted!!!\n");
-    static fnClWaitForEvents real_fn = (fnClWaitForEvents)real_dlsym(RTLD_NEXT, "clWaitForEvents");
-    return real_fn(num_events, event_list);
+    return ret;
 }
 
-cl_int clSetKernelArg(cl_kernel kernel,
+cl_int voclSetKernelArg(fnclSetKernelArg real_fn,
+        cl_kernel kernel,
         cl_uint arg_index,
         size_t arg_size,
         const void *arg_value)
 {
+    auto ret = real_fn(kernel, arg_index, arg_size, arg_value);
     fprintf(stdout, "clSetKernelArg %lx %lx\n", kernel, *(uint64_t*)arg_value);
-    static fnClSetKernelArg real_fn = (fnClSetKernelArg)real_dlsym(RTLD_NEXT, "clSetKernelArg");
-    return real_fn(kernel, arg_index, arg_size, arg_value);
+    return ret;
+}
+
+cl_mem voclCreateBuffer(fnclCreateBuffer real_fn,
+        cl_context context,
+        cl_mem_flags flags,
+        size_t size,
+        void *host_ptr,
+        cl_int *errcode_ret)
+{
+    auto ret = real_fn(context, flags, size, host_ptr, errcode_ret);
+    fprintf(stdout, "clCreateBuffer %lx\n", ret);
+    return ret;
+}
+
+cl_int voclEnqueueReadBuffer(fnclEnqueueReadBuffer real_fn,
+        cl_command_queue command_queue,
+        cl_mem buffer,
+        cl_bool blocking_read,
+        size_t offset,
+        size_t cb,
+        void *ptr,
+        cl_uint num_events_in_wait_list,
+        const cl_event *event_wait_list,
+        cl_event *event)
+{
+    auto ret = real_fn(command_queue, buffer, blocking_read, offset, cb, ptr,
+            num_events_in_wait_list, event_wait_list, event);
+    fprintf(stdout, "clEnqueueReadBuffer %lx\n", buffer);
+    return ret;
 }
--- libvocl.h
+++ libvocl.h
@@ -8,8 +8,9 @@
 #include <dlfcn.h>
 #include <CL/cl.h>
 #include <string.h>
-
-#define STRINGIFY(x) #x
+#include <boost/preprocessor/tuple/push_front.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/stringize.hpp>
 
 extern "C" {
 	void* __libc_dlsym (void *map, const char *name);
@@ -25,15 +26,95 @@ static void* real_dlsym(void *handle, const char* symbol)
 	return (*internal_dlsym)(handle, symbol);
 }
 
-typedef cl_int (*fnClEnqueueNDRangeKernel)(cl_command_queue,
-		cl_kernel, cl_uint, const size_t*, const size_t*, const size_t*,
-		cl_uint, const cl_event*, cl_event*);
+#define STRINGIFY(x) BOOST_PP_STRINGIZE(x)
+#define CONCAT(x,y) x##y
+#define FN_TYPE(x) CONCAT(fn,x)
+#define VOCL_FN(x) CONCAT(vo,x)
+#define ADD_ELEM(t, x) BOOST_PP_TUPLE_PUSH_FRONT(t, x)
+#define GET_ELEM(t, i) BOOST_PP_TUPLE_ELEM(i, t)
+#define FOR_EACH(macro, seq) BOOST_PP_SEQ_FOR_EACH(macro, _, seq)
 
-typedef cl_int (*fnClWaitForEvents)(cl_uint, const cl_event*);
+#define GET_FUNC(t) GET_ELEM(t, 1)
 
-typedef cl_int (*fnClSetKernelArg)(cl_kernel kernel,
-        cl_uint arg_index,
-        size_t arg_size,
-        const void *arg_value);
+#define WRAP_FN(ret_type, funcname, params, ...) \
+    typedef ret_type (*FN_TYPE(funcname)) params; \
+    ret_type VOCL_FN(funcname) ADD_ELEM(params, FN_TYPE(funcname) real_fn); \
+    ret_type funcname params \
+    { \
+        static auto real_fn = (FN_TYPE(funcname)) real_dlsym(RTLD_NEXT, \
+            STRINGIFY(funcname)); \
+        ret_type ret = VOCL_FN(funcname)(real_fn, __VA_ARGS__); \
+        return ret; \
+    } \
+
+
+#define INTERCEPT(r, data, ocl_call) \
+    if (strcmp(symbol, STRINGIFY(GET_FUNC(ocl_call))) == 0) { \
+        return (void*) GET_FUNC(ocl_call); \
+    } else
+
+#define WRAP(r, data, ocl_call) \
+    WRAP_FN ocl_call
+
+/*
+ * List of OpenCL calls with full signature
+ * ((return_type, function_name, (list_of_parameters), args...)
+ */
+#define OCL_CALLS \
+    ((cl_int, clEnqueueNDRangeKernel, (cl_command_queue command_queue, \
+            cl_kernel kernel, \
+            cl_uint work_dim, \
+            const size_t *global_work_offset, \
+            const size_t *global_work_size, \
+            const size_t *local_work_size, \
+            cl_uint num_events_in_wait_list, \
+            const cl_event *event_wait_list, \
+            cl_event *event), \
+        command_queue, kernel, work_dim, global_work_offset, global_work_size, \
+        local_work_size, num_events_in_wait_list, event_wait_list, event)) \
+    \
+    ((cl_int, clWaitForEvents, (cl_uint num_events, \
+            const cl_event *event_list), \
+        num_events, event_list)) \
+    \
+    ((cl_int, clSetKernelArg, (cl_kernel kernel, \
+                              cl_uint arg_index, \
+                              size_t arg_size, \
+                              const void *arg_value), \
+                              kernel, arg_index, arg_size, arg_value)) \
+    \
+    ((cl_mem, clCreateBuffer, (cl_context context, \
+            cl_mem_flags flags, \
+            size_t size, \
+            void *host_ptr, \
+            cl_int *errcode_ret), \
+        context, flags, size, host_ptr, errcode_ret)) \
+    \
+    ((cl_int, clEnqueueReadBuffer, (cl_command_queue command_queue, \
+            cl_mem buffer, \
+            cl_bool blocking_read, \
+            size_t offset, \
+            size_t cb, \
+            void *ptr, \
+            cl_uint num_events_in_wait_list, \
+            const cl_event *event_wait_list, \
+            cl_event *event), \
+        command_queue, buffer, blocking_read, offset, cb, ptr, \
+        num_events_in_wait_list, event_wait_list, event)) \
+
+/*
+ * Dynamic Intercepts
+ */
+void* dlsym(void *handle, const char *symbol)
+{
+    FOR_EACH(INTERCEPT, OCL_CALLS) {
+        return real_dlsym(handle, symbol);
+    }
+}
+
+/*
+ * Static Intercepts
+ */
+FOR_EACH(WRAP, OCL_CALLS)
 
 #endif /* _VOCL_H_ */
